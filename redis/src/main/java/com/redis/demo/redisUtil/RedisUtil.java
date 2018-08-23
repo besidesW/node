d13babc998a9e2e7765c7d93784e2ddf86ca69f5
besides <2534241865@qq.com>
2018-08-23 15:50:26 +0800

    redis 基础方法

---
+++ redis/src/main/java/com/redis/demo/redisUtil/RedisUtil.java
@@ -0,0 +1,25 @@
+package com.redis.demo.redisUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisUtil {
+
+
+    private RedisTemplate<String , Object> redisTemplate;
+
+    @Autowired(required = false)
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        this.redisTemplate = redisTemplate;
+    }
+
+}
--- redis/src/test/java/com/redis/demo/DemoApplicationTests.java
+++ redis/src/test/java/com/redis/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.redis.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redis.demo.entity.UserInfo;
-import org.junit.Assert;
+import net.sf.json.JSONObject;
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,9 +25,9 @@ public class DemoApplicationTests {
     private RedisTemplate<Object , Object> redisTemplate;
 
     @Test
-    public void testRedis(){
+    public void testString(){
         UserInfo userInfo = new UserInfo("18701744326" , "besides3" , "1234");
-        redisTemplate.opsForValue().set("user" , userInfo.getNickName());
+        redisTemplate.opsForValue().set("user2" , userInfo.getNickName());
 
         String nickNmae =(String) redisTemplate.opsForValue().get("user2");
 
@@ -33,6 +40,26 @@ public class DemoApplicationTests {
 
     }
 
+    /**
+     * redis 存储map
+     * redis 只能存入string类型的数据
+     * 需要使用json转换
+     * */
+    @Test
+    public void testHash(){
+        UserInfo userInfo = new UserInfo("18701744326" , "besides1" , "1234");
+        UserInfo userInfo2 = new UserInfo("18701744326" , "besides2" , "1234");
+        //将Userinfo 转换为json 否则无法解析
+        JSONObject json1 = JSONObject.fromObject(userInfo);
+        JSONObject json2 = JSONObject.fromObject(userInfo2);
+
+        Map<String , String> map = new HashMap();
+        map.put("user1" , json1.toString());
+        map.put("user2" , json2.toString());
+
+        redisTemplate.opsForHash().putAll("map4"  , map);
+
+    }
 
     @Test
     public void contextLoads() {
